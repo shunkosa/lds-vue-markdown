9809735a3bd71047e602c2c427f5d5412bb3591c
shunkosa <shun.kosaka.102@gmail.com>
2018-11-14 20:21:12 +0900

    Modify editor design

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,23 +1,61 @@
 <template>
-    <div class="editor">
-        <span>{{ user.displayName }}</span>
-        <h1>Editor</h1>
-        <button @click="logout">Logout</button>
-    </div>
+    <article class="slds-card slds-m-around_large">
+        <div class="slds-card__header slds-grid">
+            <header class="slds-media slds-media_center slds-has-flexi-truncate">
+                <div class="slds-media__figure">
+                    <span class="slds-icon_container slds-icon-action-edit slds-icon_container--circle">
+                        <img class="slds-icon_small" src="../assets/icons/edit_60.png" />
+                    </span>
+                </div>
+                <div class="slds-media__body">
+                    <h2 class="slds-card__header-title">
+                        <span class="slds-card__header-link">Editor</span>
+                    </h2>
+                </div>
+                <div class="slds-no-flex">
+                    <button class="slds-button slds-button_neutral" @click="logout">Logout</button>
+                </div>
+            </header>
+        </div>
+        <div class="slds-card__body slds-card__body_inner">
+            <span>logged in as : {{ user.email }}</span>
+            <div class="slds-grid slds-gutters slds-p-top_medium">
+                <div class="slds-col sld-size_1-of-6">
+                    <!--TODO: List here-->
+                </div>
+                <div class="slds-col slds-size_5-of-12">
+                    <textarea class="slds-textarea" rows="20" v-model="markdown"/>
+                </div>
+                <div class="slds-col slds-size_5-of-12">
+                    <Preview :markdown="markdown" />
+                </div>
+            </div>
+        </div>
+    </article>
 </template>
 
 <script>
-import firebase from 'firebase'
+import firebase from "firebase";
+import Preview from './Preview.vue'
 export default {
-    name: "editor",
-    props: ["user"],
-    data() {
-        return {};
-    },
-    methods: {
-        logout: function(){
-            firebase.auth().signOut();
-        }
+  name: "editor",
+  props: ["user"],
+  data() {
+    return {
+      markdown: ""
+    };
+  },
+  components:{
+      Preview: Preview
+  },
+  methods: {
+    logout: function() {
+      firebase.auth().signOut();
     }
+  }
 };
 </script>
+
+<style scoped>
+@import "../assets/slds.min.css";
+</style>
---
+++ src/components/Preview.vue
@@ -0,0 +1,23 @@
+<template>
+     <div class="markdown-body" v-html="preview()"></div> 
+</template>
+
+<script>
+import marked from 'marked'
+export default {
+    name: "preview",
+    props: ["markdown"],
+    methods: {
+    preview: function() {
+      return marked(this.markdown);
+    }
+  }
+}
+</script>
+
+<style>
+@import "../assets/github-markdown.css";
+ol, ul {
+    list-style: unset !important;
+}
+</style>
